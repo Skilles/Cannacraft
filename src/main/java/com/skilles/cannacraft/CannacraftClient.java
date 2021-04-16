54089f09e8004c0effcd1d599dfc253a995a2967
Skilles <haloeer1@gmail.com>
2021-04-16 01:32:59 -0400

    Add crop block textures

--- src/main/java/com/skilles/cannacraft/CannacraftClient.java
+++ src/main/java/com/skilles/cannacraft/CannacraftClient.java
@@ -1,9 +1,12 @@
 package com.skilles.cannacraft;
 
+import com.skilles.cannacraft.registry.ModBlocks;
 import com.skilles.cannacraft.registry.ModScreens;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 @Environment(EnvType.CLIENT)
 
@@ -11,6 +14,7 @@ public class CannacraftClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WEED_CROP, RenderLayer.getCutout());
         ModScreens.registerScreens();
         System.out.println("Screens registered!");
     }
