64d77c3cabd4f63090e401f2d322ece01f179a82
Skilles <haloeer1@gmail.com>
2021-04-18 12:16:40 -0400

    Code cleanup

--- src/main/java/com/skilles/cannacraft/registry/ModItems.java
+++ src/main/java/com/skilles/cannacraft/registry/ModItems.java
@@ -11,13 +11,11 @@ public class ModItems {
 
     // Items
     public static final Seed SEED = new Seed(ModBlocks.WEED_CROP, new Item.Settings().group(Cannacraft.ITEM_GROUP));
-    //public static final Seed SEED1 = Registry.register(Registry.ITEM, new Identifier(Cannacraft.MOD_ID, "seed_chest"), SEED);
 
     // Block items
     public static final BlockItem SEED_CHEST = new BlockItem(ModBlocks.SEED_CHEST, new Item.Settings().group(Cannacraft.ITEM_GROUP));
     public static void registerItems () {
-        //Registry.register(Registry.ITEM, new Identifier(Cannacraft.MOD_ID, "seed"), SEED);
-        Registry.register(Registry.ITEM, Cannacraft.id( "seed"), ModItems.SEED);
+        Registry.register(Registry.ITEM, Cannacraft.id( "seed"), SEED);
         Registry.register(Registry.ITEM, new Identifier(Cannacraft.MOD_ID, "seed_chest"), SEED_CHEST);
     }
 }
