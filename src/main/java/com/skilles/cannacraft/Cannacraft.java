da9d47965775b970c4bb60e232c14c8c2a48c5ae
Skilles <haloeer1@gmail.com>
2021-04-22 14:13:10 -0400

    Move commands to separate class

--- src/main/java/com/skilles/cannacraft/Cannacraft.java
+++ src/main/java/com/skilles/cannacraft/Cannacraft.java
@@ -1,34 +1,19 @@
 package com.skilles.cannacraft;
 
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.skilles.cannacraft.blocks.strainAnalyzer.AnalyzeRecipe;
 import com.skilles.cannacraft.registry.*;
 import com.skilles.cannacraft.strain.StrainMap;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeType;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Util;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
 import java.util.Optional;
-
-import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
-import static com.mojang.brigadier.arguments.StringArgumentType.getString;
-import static net.minecraft.server.command.CommandManager.argument;
-import static net.minecraft.server.command.CommandManager.literal;
 
 public class Cannacraft implements ModInitializer {
 
@@ -48,69 +33,16 @@ public class Cannacraft implements ModInitializer {
         return new Identifier(MOD_ID, path);
     }
 
-    public static int setStrain(CommandContext<ServerCommandSource> ctx, int strain) throws CommandSyntaxException {
-        final ServerPlayerEntity self = ctx.getSource().getPlayer();
-        ItemStack itemStack = self.getMainHandStack();
-        if(itemStack.getItem().equals(ModItems.WEED_SEED) || itemStack.getItem().equals(ModItems.WEED_FRUIT)){
-            //NbtCompound tag = itemStack.getOrCreateSubTag("cannacraft:strain");
-            ModComponents.STRAIN.get(itemStack).setStrain(strain); // BUG: index NBT is null when set to 0
-            //itemStack.putSubTag("cannacraft:strain", tag);
-        }
-        return 1;
-    }
-    public static int setSex(CommandContext<ServerCommandSource> ctx, String gender) throws CommandSyntaxException {
-        final ServerPlayerEntity self = ctx.getSource().getPlayer();
-        ItemStack itemStack = self.getMainHandStack();
-        if(itemStack.getItem().equals(ModItems.WEED_SEED)){
-            //NbtCompound tag = itemStack.getOrCreateSubTag("cannacraft:strain");
-            if(gender.equalsIgnoreCase("male")) { ModComponents.STRAIN.get(itemStack).setMale(true); }
-            else if(gender.equalsIgnoreCase("female")) { ModComponents.STRAIN.get(itemStack).setMale(false); }
-            else { System.out.println("Unknown gender: "+ gender); }
 
-            //itemStack.putSubTag("cannacraft:strain", tag);
-        }
-        return 1;
-    }
-    public static int identify(CommandContext<ServerCommandSource> ctx, int flag) throws CommandSyntaxException {
-        final ServerPlayerEntity self = ctx.getSource().getPlayer();
-        ItemStack handStack = self.getMainHandStack();
-        if(handStack.getItem().equals(ModItems.WEED_SEED) || handStack.getItem().equals(ModItems.WEED_FRUIT)) {
-            if(flag == 0) {
-                ModComponents.STRAIN.get(handStack).identify();
-                System.out.println("Seed identified!");
-                return 1;
-            } else {
-                int i;
-                int j = 0;
-                for(i = 0; self.getInventory().size() > i; i++) {
-                    ItemStack itemStack = self.getInventory().getStack(i);
-                    if (itemStack != null && itemStack.getItem().equals(ModItems.WEED_SEED) && !ModComponents.STRAIN.get(itemStack).identified()) {
-                        ModComponents.STRAIN.get(itemStack).identify();
-                        j++;
-                    }
-                }
-                System.out.println(j+" seeds identified!");
-            }
-        }
-        return 1;
-    }
-    public static int addStrain(CommandContext<ServerCommandSource> ctx, String name, String type) throws CommandSyntaxException {
-        final ServerPlayerEntity self = ctx.getSource().getPlayer();
-        StrainMap.addStrain(name, StrainMap.Type.valueOf(type.toUpperCase()));
-        self.sendSystemMessage(Text.of("Strain added: "+StrainMap.toStrain(name)), Util.NIL_UUID);
-        return 1;
-    }
-    public static int listStrain(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
-        final ServerPlayerEntity self = ctx.getSource().getPlayer();
-        self.sendSystemMessage(Text.of(StrainMap.getStrains().toString()), Util.NIL_UUID);
-        return 1;
-    }
     @Override
     public void onInitialize() {
-        Registry.register(Registry.RECIPE_TYPE, id("analyze"), ANALYZE_RECIPE);
-        Registry.register(Registry.RECIPE_SERIALIZER, id("analyze"), ANALYZE_RECIPE_SERIALIZER);
+        //Registry.register(Registry.RECIPE_TYPE, id("analyze"), ANALYZE_RECIPE);
+        //Registry.register(Registry.RECIPE_SERIALIZER, id("analyze"), ANALYZE_RECIPE_SERIALIZER);
 
         StrainMap.registerStrains();
+
+        ModCommands.registerCommands();
+        System.out.println("Commands registered!");
 
         LootTable.registerLoot();
         System.out.println("LootTables registered!");
@@ -127,42 +59,6 @@ public class Cannacraft implements ModInitializer {
         ModScreens.registerScreenHandlers();
         System.out.println("ScreenHandlers registered!");
 
-        CommandRegistrationCallback.EVENT.register((dispatcher, integrated) -> {
-            dispatcher.register(literal("strain")
-                    .then(literal("identify")
-                            .executes(ctx -> {
-                                identify(ctx, 0);
-                                return 1;
-                                }).then(literal("all")
-                                    .executes(ctx -> {
-                                    identify(ctx, 1);
-                                    return 1;
-                                    })))
-                    .then(literal("set")
-                            .then(argument("index", IntegerArgumentType.integer(0, StrainMap.getStrainCount()))
-                                .executes(ctx -> {
-                                    System.out.println("Seed strain set!");
-                                    setStrain(ctx, getInteger(ctx, "index"));
-                                    return 1;
-                                })))
-                    .then(literal("gender")
-                            .then(argument("sex", StringArgumentType.string()).executes(ctx -> {
-                                setSex(ctx, getString(ctx,"sex"));
-                                return 1;
-                            })))
-                    .then(literal("add")
-                            .then(argument("name", StringArgumentType.string())
-                                    .then(argument("type", StringArgumentType.string())
-                                        .executes(ctx -> {
-                                        addStrain(ctx, getString(ctx, "name"), getString(ctx, "type"));
-                                    return 1;
-                                }))))
-                    .then(literal("list")
-                            .executes(ctx -> {
-                                listStrain(ctx);
-                                return 1;
-                            }))
-            );
-        });
+
     }
 }
