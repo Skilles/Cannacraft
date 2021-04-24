88a6babb16f2567b58adaf26a0513a649c4c5eb3
Skilles <haloeer1@gmail.com>
2021-04-24 15:04:50 -0400

    Add power to machines New abstract machine block and entity

--- src/main/java/com/skilles/cannacraft/blocks/strainAnalyzer/StrainAnalyzerEntity.java
+++ src/main/java/com/skilles/cannacraft/blocks/strainAnalyzer/StrainAnalyzerEntity.java
@@ -1,50 +1,65 @@
 package com.skilles.cannacraft.blocks.strainAnalyzer;
 
+import com.skilles.cannacraft.blocks.MachineBlockEntity;
 import com.skilles.cannacraft.registry.ModEntities;
 import com.skilles.cannacraft.registry.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
-import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import team.reborn.energy.Energy;
+import team.reborn.energy.EnergySide;
+import team.reborn.energy.EnergyTier;
 
-public class StrainAnalyzerEntity extends BlockEntity implements SidedInventory, NamedScreenHandlerFactory {
+public class StrainAnalyzerEntity extends MachineBlockEntity {
 
+
+    private double powerMultiplier = 1; // Energy use multiplier
+    private boolean needsPower = true;
     private int processingTime;
-    public DefaultedList<ItemStack> inventory;
+    private int powerStored;
     private final PropertyDelegate propertyDelegate;
 
     public StrainAnalyzerEntity(BlockPos pos, BlockState state) {
-        super(ModEntities.STRAIN_ANALYZER_ENTITY, pos, state);
-        this.inventory = DefaultedList.ofSize(2, ItemStack.EMPTY);
+        super(ModEntities.STRAIN_ANALYZER_ENTITY, pos, state, DefaultedList.ofSize(2, ItemStack.EMPTY));
         this.propertyDelegate = new PropertyDelegate() {
             @Override
             public int get(int index) {
-                return StrainAnalyzerEntity.this.processingTime;
+                switch(index) {
+                    case 0:
+                        return StrainAnalyzerEntity.this.processingTime;
+                    case 1:
+                        return StrainAnalyzerEntity.this.powerStored;
+                    default:
+                        return 0;
+                }
             }
 
             @Override
             public void set(int index, int value) {
-                StrainAnalyzerEntity.this.processingTime = value;
+                switch(index) {
+                    case 0:
+                        StrainAnalyzerEntity.this.processingTime = value;
+                    case 1:
+                        StrainAnalyzerEntity.this.powerStored = value;
+                }
             }
 
             @Override
             public int size() {
-                return 1;
+                return 2;
             }
         };
     }
@@ -52,11 +67,14 @@ public class StrainAnalyzerEntity extends BlockEntity implements SidedInventory,
 
     public static void tick(World world, BlockPos pos, BlockState state, StrainAnalyzerEntity blockEntity) {
         if (world == null || world.isClient) return;
+        if(isNextTo(world, pos, Blocks.GLOWSTONE) && blockEntity.powerStored < blockEntity.getMaxStoredPower()) {
+            blockEntity.addEnergy(2);
+        }
         if (blockEntity.isWorking()) {
             if (canCraft(blockEntity.inventory) && blockEntity.processingTime == 184) {
                 craft(blockEntity.inventory);
-
                 blockEntity.processingTime = 1;
+                //blockEntity.propertyDelegate.set(0, 1);
                 markDirty(world, pos, state);
             } else if (!canCraft(blockEntity.inventory)) {
                 blockEntity.processingTime = 0;
@@ -67,8 +85,9 @@ public class StrainAnalyzerEntity extends BlockEntity implements SidedInventory,
             world.setBlockState(pos, state, Block.NOTIFY_ALL);
             markDirty(world, pos, state);
 
+
             if (!world.isReceivingRedstonePower(pos)) {
-                blockEntity.processingTime++;
+                processTick(blockEntity);
             }
 
         } else if (canCraft(blockEntity.inventory)) {
@@ -81,28 +100,15 @@ public class StrainAnalyzerEntity extends BlockEntity implements SidedInventory,
             world.setBlockState(pos, state, Block.NOTIFY_ALL);
             markDirty(world, pos, state);
         }
-    }
 
-    @Override
-    public NbtCompound writeNbt(NbtCompound nbt) {
-        super.writeNbt(nbt);
-        nbt.putInt("processingTime", this.processingTime);
-        Inventories.writeNbt(nbt, this.inventory);
-        return nbt;
     }
-
-    @Override
-    public void readNbt(NbtCompound nbt) {
-        super.readNbt(nbt);
-        this.inventory = DefaultedList.ofSize(this.inventory.size(), ItemStack.EMPTY);
-        Inventories.readNbt(nbt, this.inventory);
-        this.processingTime = nbt.getInt("processingTime");
-    }
-
     public boolean isWorking() {
-        return processingTime != 0;
+        if(needsPower) {
+            return processingTime != 0 && powerStored != 0;
+        } else {
+            return processingTime != 0;
+        }
     }
-
     public static boolean canCraft(DefaultedList<ItemStack> inventory) {
             ItemStack stack = inventory.get(1);
             ItemStack output = inventory.get(0);
@@ -115,16 +121,19 @@ public class StrainAnalyzerEntity extends BlockEntity implements SidedInventory,
             }
         return false;
     }
-
+    private static void processTick(StrainAnalyzerEntity blockEntity) {
+        blockEntity.processingTime++;
+        if(blockEntity.needsPower) blockEntity.useEnergy(1 * blockEntity.powerMultiplier);
+    }
     public static void craft(DefaultedList<ItemStack> inventory) {
 
             ItemStack stack = inventory.get(1);
             NbtCompound tag = stack.getTag().copy();
-            ItemStack outputSlot = inventory.get(0).copy();
+            ItemStack outputSlot = inventory.get(0);
             ItemStack output = ModItems.WEED_SEED.getDefaultStack();
 
 
-            if(tag != null && outputSlot.isEmpty()) {
+            if(outputSlot.isEmpty()) {
                 NbtCompound strainTag = tag.getCompound("cannacraft:strain").copy();
                 strainTag.putBoolean("Identified", true);
                 NbtCompound outputTag = new NbtCompound();
@@ -138,83 +147,80 @@ public class StrainAnalyzerEntity extends BlockEntity implements SidedInventory,
             stack.decrement(1);
 
     }
+    @Override
+    public double getStored(EnergySide face) {
+        return this.powerStored;
+    }
 
     @Override
-    public int[] getAvailableSlots(Direction side) {
-        if (side == Direction.DOWN) {
-            return new int[] {0};
-        } else {
-            return new int[] {1};
+    public void setStored(double amount) { powerStored = (int) amount; }
+    public void addEnergy(double amount) {
+        setStored(powerStored + amount);
+    }
+    public double getEnergy() {
+        return getStored(EnergySide.UNKNOWN);
+    }
+    public void useEnergy(double amount) {
+        if (amount > powerStored) amount = powerStored;
+            setStored(powerStored - amount);
+    }
+    public void setEnergy(double amount) {
+        setStored(amount);
+    }
+    public void sideTransfer(World world, BlockPos pos, BlockEntity blockEntity) {
+        for (Direction side : Direction.values()) {
+            BlockEntity sideBlockEntity = world.getBlockEntity(pos.offset(side));
+            if (sideBlockEntity == null || !Energy.valid(sideBlockEntity)) {
+                continue;
+            }
+            Energy.of(blockEntity)
+                    .side(side)
+                    .into(Energy.of(sideBlockEntity).side(side.getOpposite()))
+                    .move();
         }
     }
-
-    @Override
-    public boolean canInsert(int slot, ItemStack stack, @Nullable Direction dir) {
-        return stack.isOf(ModItems.WEED_SEED);
-    }
-
-    @Override
-    public boolean canExtract(int slot, ItemStack stack, Direction dir) {
-        return dir == Direction.DOWN && slot == 0;
-    }
-
-    @Override
-    public int size() {
-        return this.inventory.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return this.inventory.isEmpty();
-    }
-
-    @Override
-    public ItemStack getStack(int slot) {
-        //return this.inventory.get(slot);
-        return slot >= 0 && slot < this.inventory.size() ? this.inventory.get(slot) : ItemStack.EMPTY;
-    }
-
-    @Override
-    public ItemStack removeStack(int slot, int amount) {
-        return Inventories.splitStack(this.inventory, slot, amount);
-    }
-
-    @Override
-    public ItemStack removeStack(int slot) {
-        return Inventories.removeStack(this.inventory, slot);
-    }
-
-    @Override
-    public void setStack(int slot, ItemStack stack) {
-        if (slot >= 0 && slot < this.inventory.size()) {
-            this.inventory.set(slot, stack);
+    protected static boolean isNextTo(World world, BlockPos pos, Block block) {
+        for (Direction side : Direction.values()) {
+            Block sideBlock = world.getBlockState(pos.offset(side)).getBlock();
+            if (sideBlock == block) {
+                return true;
+            }
         }
+        return false;
     }
-
-
-    @Override
-    public boolean canPlayerUse(PlayerEntity player) {
-        //noinspection ConstantConditions
-        if (this.world.getBlockEntity(this.pos) != this) {
-            return false;
-        } else {
-            return player.squaredDistanceTo((double)this.pos.getX() + 0.5D, (double)this.pos.getY() + 0.5D, (double)this.pos.getZ() + 0.5D) <= 64.0D;
-        }
-    }
-
-    @Override
-    public void clear() {
-        this.inventory.clear();
-    }
-
-    @Override
-    public Text getDisplayName() {
-        return new TranslatableText(getCachedState().getBlock().getTranslationKey());
-    }
-
     @Nullable
     @Override
     public ScreenHandler createMenu(int syncId, PlayerInventory inv, PlayerEntity player) {
         return new StrainAnalyzerScreenHandler(syncId, inv, this, this.propertyDelegate);
     }
+
+    @Override
+    public double getMaxStoredPower() {
+        return 10000;
+    }
+
+    @Override
+    public EnergyTier getTier() {
+        return EnergyTier.LOW;
+    }
+
+
+    @Override
+    public NbtCompound writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+        nbt.putInt("processingTime", this.processingTime);
+        nbt.putInt("powerStored", this.powerStored);
+        Inventories.writeNbt(nbt, this.inventory);
+        return nbt;
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+        this.inventory = DefaultedList.ofSize(this.inventory.size(), ItemStack.EMPTY);
+        Inventories.readNbt(nbt, this.inventory);
+        this.processingTime = nbt.getInt("processingTime");
+        this.powerStored = nbt.getInt("powerStored");
+    }
+
 }
--- src/main/java/com/skilles/cannacraft/blocks/strainAnalyzer/StrainAnalyzerScreen.java
+++ src/main/java/com/skilles/cannacraft/blocks/strainAnalyzer/StrainAnalyzerScreen.java
@@ -8,7 +8,9 @@ import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 @Environment(EnvType.CLIENT)
 public class StrainAnalyzerScreen extends HandledScreen<StrainAnalyzerScreenHandler> {
@@ -17,7 +19,7 @@ public class StrainAnalyzerScreen extends HandledScreen<StrainAnalyzerScreenHand
     public StrainAnalyzerScreen(StrainAnalyzerScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
     }
-
+    int newEnergy = 0;
     @Override
     protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
@@ -27,11 +29,24 @@ public class StrainAnalyzerScreen extends HandledScreen<StrainAnalyzerScreenHand
         int j = this.field_2800;
         this.drawTexture(matrices, i, j, 0, 0, this.backgroundWidth, this.backgroundHeight);
         int l = this.handler.processingTime() / 8;
-        this.drawTexture(matrices, i + 78, j + 31, 177, 7, l, 23);
+        int energy = this.handler.powerStored() / 161; // 62 max
+        this.drawTexture(matrices, i + 78, j + 31, 177, 7, l, 23); // arrow
+
+        // power bar
+        this.drawTexture(matrices, i + 150, j + 9,177, 33, 18, 62);
+        this.drawTexture(matrices, i + 151, j + 9, 151, 9, 16, 62-energy);
+
+        if(energy != newEnergy) {
+            System.out.println(energy);
+        }
+        newEnergy = energy;
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        if(mouseX >= 275 && mouseX <= 292 && mouseY >= 45 && mouseY <= 107) { // if hovering over power bar
+            renderTooltip(matrices, new LiteralText("Energy: "+this.handler.powerStored()).formatted(Formatting.GOLD), field_2776 + 168, field_2800 + 20);
+        }
         this.renderBackground(matrices);
         super.render(matrices, mouseX, mouseY, delta);
         this.drawMouseoverTooltip(matrices, mouseX, mouseY);
