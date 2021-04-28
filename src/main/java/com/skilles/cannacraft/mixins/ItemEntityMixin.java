78b02c75c37be6ee8478f1aa0566e32c9d7467c4
Skilles <haloeer1@gmail.com>
2021-04-27 20:19:09 -0400

    Code cleanup

--- src/main/java/com/skilles/cannacraft/mixins/ItemEntityMixin.java
+++ src/main/java/com/skilles/cannacraft/mixins/ItemEntityMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
- * Catches dropped items and assigns random thc if needed.
+ * Catches dropped items and assigns random thc and ID if needed.
  * In the future, can do some custom behavior with dropped items...
  */
 
@@ -24,13 +24,12 @@ public abstract class ItemEntityMixin {
     @Shadow public abstract ItemStack getStack();
 
     @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/world/World;DDDLnet/minecraft/item/ItemStack;)V")
-    public void ItemEntity(World world, double x, double y, double z, ItemStack stack, CallbackInfo ci) throws Exception {
+    public void ItemEntity(World world, double x, double y, double z, ItemStack stack, CallbackInfo ci) {
         if(stack.getItem().equals(ModItems.WEED_SEED) || stack.getItem().equals(ModItems.WEED_FRUIT)) {
             NbtCompound tag = stack.getSubTag("cannacraft:strain");
             if(tag != null && tag.contains("ID")) {
                 if(tag.getInt("ID") == 0) tag.putInt("ID", GeneticsManager.random().nextInt(StrainMap.ogStrainCount - 1) + 1);
                 if(!tag.contains("THC")) tag.putInt("THC", StrainMap.normalDist(15, 5, 13));
-                //this.getStack().putSubTag("cannacraft:strain", tag);
             }
         }
     }
