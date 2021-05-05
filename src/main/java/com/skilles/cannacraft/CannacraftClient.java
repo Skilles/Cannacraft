6ea400eb50aec20cd2ff70f8730528a1cbaa918d
Skilles <haloeer1@gmail.com>
2021-05-05 09:16:25 -0400

    Add config
    ModMenu integration

--- src/main/java/com/skilles/cannacraft/CannacraftClient.java
+++ src/main/java/com/skilles/cannacraft/CannacraftClient.java
@@ -1,7 +1,11 @@
 package com.skilles.cannacraft;
 
 import com.skilles.cannacraft.registry.ModBlocks;
+import com.skilles.cannacraft.registry.ModConfig;
 import com.skilles.cannacraft.registry.ModScreens;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -10,12 +14,16 @@ import net.minecraft.client.render.RenderLayer;
 
 @Environment(EnvType.CLIENT)
 
-public class CannacraftClient implements ClientModInitializer {
-
+public class CannacraftClient implements ClientModInitializer, ModMenuApi  {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(ModConfig.class, ModConfig.currentScreen).get();
+    }
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WEED_CROP, RenderLayer.getCutout());
         ModScreens.registerScreens();
+        ModConfig.registerConfig();
         System.out.println("Screens registered!");
     }
 }
---
+++ src/main/java/com/skilles/cannacraft/registry/ModConfig.java
@@ -0,0 +1,58 @@
+package com.skilles.cannacraft.registry;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+
+@Config(name = "cannacraft")
+public class ModConfig implements ConfigData {
+    public static Screen currentScreen = MinecraftClient.getInstance().currentScreen;
+
+
+    boolean australia = false;
+    boolean spread = false;
+    boolean debug = false;
+
+    @ConfigEntry.Gui.CollapsibleObject
+    InnerStuff crop = new InnerStuff();
+
+    static class InnerStuff {
+        @ConfigEntry.Gui.Tooltip
+        float speed = 1.0F;
+        @ConfigEntry.Gui.Tooltip
+        int yield = 1;
+    }
+
+    @Override
+    public void validatePostLoad() throws ValidationException {
+        if(crop.speed > 4.0F) {
+            crop.speed = 4.0F;
+        } else if(crop.speed < 0) {
+            crop.speed = 0;
+        }
+        ConfigData.super.validatePostLoad();
+    }
+    public float getSpeed() {
+        return crop.speed;
+    }
+    /*public static Screen configScreen() {
+            ConfigBuilder builder= ConfigBuilder.create()
+                    .setParentScreen(ModConfig.currentScreen)
+                    .setTitle(new TranslatableText("title.cannacraft.config"));
+            ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("category.cannacraft.general"));
+            ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+            general.addEntry(entryBuilder.startStrField(new TranslatableText("option.cannacraft.optionA"), "test")
+                    .setDefaultValue("This is the default value") // Recommended: Used when user click "Reset"
+                    .setTooltip(new TranslatableText("This option is awesome!")) // Optional: Shown when the user hover over this option
+                    //.setSaveConsumer(newValue -> currentValue = newValue) // Recommended: Called when user save the config
+                    .build()); // Builds the option entry for cloth config
+            return builder.setFallbackCategory(general).build();
+        }*/
+    public static void registerConfig() {
+        AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+    }
+}
